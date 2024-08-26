d8de0ef83df700bd65bbc8fe6fd22ed534456cd9
JustJJaye <jijustin@hotmail.com>
2024-08-25 22:26:47 -0700

    Initial Setup of Two Rooms and a Boom

---
+++ src/main/java/com/example/TwoRoomsBoom/TwoRoomsBoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.TwoRoomsBoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoRoomsBoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoRoomsBoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TwoRoomsBoom/TwoRoomsBoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TwoRoomsBoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoRoomsBoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
