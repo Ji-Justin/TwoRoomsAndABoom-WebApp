2dfef72b65a51ab1142585d4fb6eed1faef93645
Riley Champion <rchamp2u@gmail.com>
2024-09-02 16:01:31 -0400

    Create Chat and Room classes

---
+++ src/main/java/com/example/TwoRoomsBoom/models/Chat.java
@@ -0,0 +1,18 @@
+package com.example.TwoRoomsBoom.models;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Chat {
+    private List<Message> history;
+}
---
+++ src/main/java/com/example/TwoRoomsBoom/models/Message.java
@@ -0,0 +1,17 @@
+package com.example.TwoRoomsBoom.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private Player player;
+    private String text;
+}
